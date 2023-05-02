439c57da5d81d73e69bfecb7f62c87bfc392c1e9
Ignacio <ignacio.becher@gmail.com>
2023-05-02 16:23:21 -0300

    qualifications in homepage working

--- js/logedHome.js
+++ js/logedHome.js
@@ -1,10 +1,19 @@
 const userId = localStorage.getItem("userId");
-console.log("El user id es:" + userId);
+let body = document.querySelector("body");
 
 const getQualifications = async () => {
-  const res = await fetch(`http://localhost:3001/students/${userId}`);
+  const res = await fetch(`http://localhost:3001/users/userHome/${userId}`);
   const qualifications = await res.json();
-  console.log(qualifications);
+  const grades = qualifications.data.gradesId;
+  for (let i = 0; i < grades.length; i++) {
+    let div = document.createElement("div");
+    div.textContent = grades[i].subject + grades[i].qualification;
+    body.appendChild(div);
+    // console.log(grades[i].subject);
+    // console.log(grades[i].qualification);
+  }
 };
 
-getQualifications();
+window.onload = () => {
+  getQualifications();
+};
