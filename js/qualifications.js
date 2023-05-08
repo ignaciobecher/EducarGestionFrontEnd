24cf8081685e1ade36c4e4e3bf95f36616dbbcae
Ignacio <ignacio.becher@gmail.com>
2023-05-08 09:26:07 -0300

    qualis done, needs fixes

--- js/qualifications.js
+++ js/qualifications.js
@@ -10,13 +10,28 @@ const getGrades = async () => {
   qualis = res.data.gradesId; //array de objetos
 
   const subject = qualis.map((obj) => obj.subject); //array de materias
+  const qualifications = qualis.map((obj) => obj.qualification); //array de calificaciones
+
+  const tableBody = document.getElementById("t-body"); // Obtener la referencia al cuerpo de la tabla
 
   for (let i = 0; i < subject.length; i++) {
     const subjects = subject[i];
-    const element = document.createElement("td");
-    element.textContent = subjects;
 
-    tbody.appendChild(element);
+    // Crear una nueva fila
+    const row = document.createElement("tr");
+
+    // Crear la columna para el subject
+    const subjectColumn = document.createElement("td");
+    subjectColumn.textContent = subjects;
+    row.appendChild(subjectColumn);
+
+    // Crear una nueva columna para la calificación
+    const qualificationColumn = document.createElement("td");
+    qualificationColumn.textContent = qualifications[i];
+    row.appendChild(qualificationColumn);
+
+    // Agregar la fila al cuerpo de la tabla
+    tableBody.appendChild(row);
   }
 };
 
