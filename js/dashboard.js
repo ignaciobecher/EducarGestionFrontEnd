62b5ca69025426a785e80722ef75e56327d4474b
Ignacio <ignacio.becher@gmail.com>
2023-05-13 21:18:37 -0300

    disable and enable buttons working

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,13 +1,13 @@
 const userId = localStorage.getItem("userId");
 const mainTittle = document.getElementById("mainTittle");
 const mainContent = document.getElementById("main-contents");
+
+//FUNCION NAVBAR
 $(".nav-toggle").click(function (e) {
   e.preventDefault();
   $("html").toggleClass("openNav");
   $(".nav-toggle").toggleClass("active");
 });
-
-console.log(userId);
 
 //INFORMACION DE INICIO
 const showMainInfo = async () => {
@@ -21,25 +21,53 @@ const showPersonalInfo = async () => {
   const res = await fetch(`http://localhost:3001/users/userHome/${userId}`);
   const userData = await res.json();
   mainContent.innerHTML = `
-<div class="mis-datos">
-  <h2>Mis datos</h2>
-  <p>Nombre: <input type="text" id="name" value="" /></p>
-  <p>
-    Apellido:
-    <input type="text" id="surname" value="" />
-  </p>
+  <div class="mis-datos">
+        <h2>Mis datos</h2>
+        <p>Nombre: <input type="text" id="name" value="${userData.data.name}" /></p>
+        <p>
+          Apellido:
+          <input type="text" id="surname" value="${userData.data.surname}" />
+        </p>
 
-  <p>Direccion: <input type="text" id="address" value="" /></p>
-  <p>Telefono: <input type="text" id="phone" value="" /></p>
-  <p>Institucion: <input type="text" id="school" value="" /></p>
+        <p>Direccion: <input type="text" id="address" value="${userData.data.address}" /></p>
+        <p>Telefono: <input type="text" id="phone" value="${userData.data.phone}" /></p>
+        <p>Institucion: <input type="text" id="school" value="" /></p>
 
-  <p>
-    Edad:
-    <input type="text" id="age" value="" />
-  </p>
-  <button id="edit-info">Editar</button>
-  <button id="save-info">Guardar</button>
-</div>`;
+        <p>
+          Edad:
+          <input type="text" id="age" value="${userData.data.age}" />
+        </p>
+        <button id="edit-info">Editar</button>
+        <button id="save-info">Guardar</button>
+    </div>`;
+  //SELECCION DE LOS BOTONES
+  const btnSave = document.getElementById("save-info");
+  const btnEdit = document.getElementById("edit-info");
+
+  //FUNCION DESHABILITAR INPUTS
+  const disableInputs = () => {
+    const inputs = document.querySelectorAll(".mis-datos input");
+    inputs.forEach((input) => {
+      input.disabled = true;
+    });
+  };
+
+  //FUNCION DESHABILITAR INPUTS
+  const enableInputs = () => {
+    const inputs = document.querySelectorAll(".mis-datos input");
+    inputs.forEach((input) => {
+      input.disabled = false;
+    });
+  };
+
+  //DESHABILITAR INPUTS
+  btnSave.addEventListener("click", () => {
+    disableInputs();
+  });
+  //HABILITAR BOTONES
+  btnEdit.addEventListener("click", () => {
+    enableInputs();
+  });
 };
 
 window.addEventListener("load", () => {
